26baf1cd8eb055dbf65949e882e1edb29bee5bdf
sagedurivage <sagedurivage@gmail.com>
2024-01-28 11:02:36 -0800

    diplay header: Flights.js display header: Listings.js

--- src/pages/Flights/Flights.js
+++ src/pages/Flights/Flights.js
@@ -2,30 +2,34 @@ import React from "react";
 import data from "./data2";
 import Card from "./Card";
 import styles from './Flights.module.css';
+import Header from '../../components/Header/Header';
 
 
 function flightCard(property) {
     return (
-        <div>
-            <Card
-                total = {property.priceBreakdown.total.units}
-                currCode = {property.priceBreakdown.total.currencyCode}
-                segments = {property.segments}
-            />
-        </div>
+        <Card
+            total = {property.priceBreakdown.total.units}
+            currCode = {property.priceBreakdown.total.currencyCode}
+            segments = {property.segments}
+        />
     );
 }
 
-function Flights(){
+function Flights() {
     return (
-        <div>
-        <h1 className={styles.listingsTitle}>Flights</h1>
-        <div className={styles.container}>
-            
-            {/* Iterate over 'data' to render each item */}
-            {data.data.flightOffers.map(flightCard)}
+        <body>
+            <div>
+                <Header />
+                <div>
+                    <h1 className={styles.flightsTitle}>Flights</h1>
+                    <div className={styles.container}>
+                        
+                        {/* Iterate over 'data' to render each item */}
+                        {data.data.flightOffers.map(flightCard)}
+                    </div>
+                </div>
             </div>
-            </div>
+        </body>
     );
 }
 
--- src/pages/Listings/Listings.js
+++ src/pages/Listings/Listings.js
@@ -3,38 +3,43 @@ import ReactDOM from 'react-dom';
 import styles from './Listings.module.css';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import Header from '../../components/Header/Header';
 
 import data from './data';
 import Card from "./Card";
 
   
 
-    function Listings(){
-      const location = useLocation();
-      const total = location.state.flightCost;
-      function createCard(property) {
-        return (
-          <Card
-            name={property.name}
-            address={property.address}
-            img1={property.images[0]}
-            type={property.roomType}
-            beds={property.beds}
-            baths={property.bathrooms}
-            price={property.price.total}
-            total = {total}
-          />
-        );
-      }
-      return (
-                <div>
-                    <h1 className={styles.listingsTitle}>Listings</h1>
-                    <div className={styles.container}>
-                      {data.results.map(createCard)}
-                    </div>
-
-                    </div>
-            );
-        }
+function Listings(){
+  const location = useLocation();
+  const total = location.state.flightCost;
+  function createCard(property) {
+    return (
+      <Card
+        name={property.name}
+        address={property.address}
+        img1={property.images[0]}
+        type={property.roomType}
+        beds={property.beds}
+        baths={property.bathrooms}
+        price={property.price.total}
+        total = {total}
+      />
+    );
+  }
+  return (
+    <body>
+      <div>
+        <Header />
+        <div>
+        <h1 className={styles.listingsTitle}>Listings</h1>
+          <div className={styles.container}>
+            {data.results.map(createCard)}
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
         
 export default Listings;
