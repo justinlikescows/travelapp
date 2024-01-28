a5aac08ce3f42c77c1857c5bb405c6f26e096068
jmpham3 <jmpham3@uci.edu>
2024-01-28 00:00:33 -0800

    ???

--- src/pages/Listings/Card.js
+++ src/pages/Listings/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import Detail from "./Detail";
 import styles from './Listings.module.css';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 
 function Card(props) {
@@ -12,7 +13,9 @@ function Card(props) {
               <p>{props.address}</p>
               <p>{props.type} with {props.beds} beds and {props.baths} bathrooms for {props.capacity} people.</p>
               <p >Price: ${props.price}</p>
-            <Button className={styles.mybutton}>Book now</Button>
+              <Link to="/checkout">
+              <Button className={styles.mybutton}>Book now</Button>
+              </Link>
       </div>
     );
   }
